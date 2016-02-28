012342fcf2e98824d3d8dfd4db5c6b3645043a88
David Lizárraga <dlizarra5@gmail.com>
2016-02-28 19:10:49 +0100

    Package name refactoring

--- src/main/frontend/app/index.js
+++ src/main/frontend/app/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-	<h1>App working</h1>,
+	<h1 class="colorred">App working</h1>,
 	document.querySelector('.container'));
