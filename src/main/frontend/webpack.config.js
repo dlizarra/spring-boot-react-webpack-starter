1246dcbbf513e5f8c3d5593ea33b611426e1f386
David Lizárraga <dlizarra5@gmail.com>
2016-03-12 20:30:35 +0100

    Tweaked webpack devserver

--- src/main/frontend/webpack.config.js
+++ src/main/frontend/webpack.config.js
@@ -1,26 +1,35 @@
+var path = require("path");
+var webpack = require('webpack');
+
 module.exports = {
-  entry: [
-	"./app/index.js"
-  ],
-  output: {
-    path: '../../../target/classes/static',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
-  module: {
-  	loaders: [{
-  	  exclude: /node_modules/,
-  	  loader: 'babel'  		
-  	}, {
-      test: /\.css$/,
-      loader: 'style!css'
-    }]
-  },
-  resolve: {
-  	extensions: ['', '.js', '.jsx']
-  },
-  devServer: {
-  	historyApiFallback: true,
-  	contentBase: './'
-  }
+    entry: [
+        "./app/index.js",
+        //'webpack-dev-server/client?http://0.0.0.0:9090',
+        //'webpack/hot/only-dev-server'
+    ],
+    output: {
+        path: path.resolve(__dirname, '../../../target/classes/static'),
+        //publicPath: 'http://localhost:9090/assets/',
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel'
+        }, {
+            test: /\.css$/,
+            loader: 'style!css'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devtool: 'source-map',
+    //devServer: {
+    //    historyApiFallback: true
+    //}
 };
